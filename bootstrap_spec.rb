0dbee32c01183b743202b2055384469b68e4036d
André Formento <andreformento.sc@gmail.com>
2017-08-08 23:13:10 -0300

    Add test shipping label for product with message

--- bootstrap_spec.rb
+++ bootstrap_spec.rb
@@ -14,4 +14,10 @@ describe ShippingLabelGenerator do
 
     expect(result).to eq("generating shipping label for product Iron Maiden - The Trooper ")
   end
+
+  it "produce shipping label for product with message" do
+    result = @shipping_label_generator.produce(@product, "Item isento de impostos...")
+
+    expect(result).to eq("generating shipping label for product Iron Maiden - The Trooper Item isento de impostos...")
+  end
 end
